47dea8a0f4b6a4787ccceb2a76502a8a9b04ea98
Kushen De Silva <60555833+kushenthimira@users.noreply.github.com>
2021-12-16 15:24:25 +0530

    build v2.2
    -New launcher icon

--- android/app/src/main/kotlin/com/cipher/flutter_weather/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cipher.flutter_weather
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/weathertoday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weathertoday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
